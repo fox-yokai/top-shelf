162b3b869640de251111c1cb3dc2a9d0940b9af6
fox-yokai <pat.w.cain@gmail.com>
2020-04-13 16:34:27 -0500

    initilized sequelize and created user schema in user.js

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,11 @@
 DROP DATABASE IF EXISTS topshelf_db;
 -- Creates the topshelf_db database --
 CREATE DATABASE topshelf_db;
+
+CREATE TABLE IF NOT EXISTS `Users` (
+`id` INTEGER NOT NULL auto_increment , 
+`email` VARCHAR(255) NOT NULL UNIQUE, 
+`password` VARCHAR(255) NOT NULL, 
+`createdAt` DATETIME NOT NULL, 
+`updatedAt` DATETIME NOT NULL, 
+PRIMARY KEY (`id`)
---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var db = require("./models");
+
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+        console.log("Listening on port %s", PORT);
+    });
+});
