646639156a1233e880708ca7dfa259e53c606aa8
fox-yokai <pat.w.cain@gmail.com>
2020-04-14 11:51:21 -0500

    created winesController

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(require("./controllers/winesController"))
+
 db.sequelize.sync().then(function() {
     app.listen(PORT, function() {
         console.log("Listening on port %s", PORT);
