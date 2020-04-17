1fbc9b12c9732814004dad93a55c5a2615d39eef
frunox <60527588+frunox@users.noreply.github.com>
2020-04-17 16:28:16 -0500

    event listener working

---
+++ public/js/members-showDetails.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+
+    // shows the add wine panel
+    $(document).on('click', '.moreBtn', function () {
+        $("#show-wine-details").show();
+    });
+
+});
--- public/js/members.js
+++ public/js/members.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var $noteList = $(".list-container .list-group");
+  var $wineList = $(".list-container .list-group");
   $.get("/api/user_data").then(function (data) {
     $(".member-name").text(data.email);
     const userid = data.id;
@@ -10,14 +10,15 @@ $(document).ready(function () {
         for (let i = 0; i < response.length; i++) {
           console.log(response[i].name)
           const { id, name, variety, location, color, winery, year, numBottles, UserId } = response[i];
+          var $moreBtn = $("<button type='button' class='btn btn-info btn-sm moreBtn'>More</button>")
           var $li = $("<li class='list-group-item'>");
           $li.attr("id", i);
           var $div1 = $("<div>").append("Name:  ", response[i].name)
           var $div2 = $("<div>").append("Year:  ", response[i].year);
           var $div3 = $("<div>").append("Winery:  ", response[i].winery);
-          $li.append($div1, $div2, $div3);
-          $noteList.append($li);
-
+          var $div4 = $("<div>").append($moreBtn)
+          $li.append($div1, $div2, $div3, $div4);
+          $wineList.append($li);
         }
       })
   });
