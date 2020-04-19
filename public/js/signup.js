19cfc21ab41feadeda899ab2b388b390bdf46597
frunox <60527588+frunox@users.noreply.github.com>
2020-04-19 13:10:07 -0500

    redirect working, fixed navbar error by reverting to jquery 3.4.1

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,37 +1,37 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
- var signUpForm = $("form.signup");
- var emailInput = $("input#email-input");
- var passwordInput = $("input#password-input");
+    var signUpForm = $("form.signup");
+    var emailInput = $("input#email-input");
+    var passwordInput = $("input#password-input");
 
- signUpForm.on("submit", function(event) {
-     event.preventDefault();
-     console.log(event);
-     var userData = {
-         email: emailInput.val().trim(),
-         password: passwordInput.val().trim()
-     };
+    signUpForm.on("submit", function (event) {
+        event.preventDefault();
+        console.log(event);
+        var userData = {
+            email: emailInput.val().trim(),
+            password: passwordInput.val().trim()
+        };
 
-     if (!userData.email || !userData.password) {
-        return;
-      }
-      // If we have an email and password, run the signUpUser function
-      signUpUser(userData.email, userData.password);
-      emailInput.val("");
-      passwordInput.val("");
- });
+        if (!userData.email || !userData.password) {
+            return;
+        }
+        // If we have an email and password, run the signUpUser function
+        signUpUser(userData.email, userData.password);
+        emailInput.val("");
+        passwordInput.val("");
+    });
 
 
- function signUpUser(email, password) {
-     $.post("/api/signup", {
-         email: email,
-         password: password
-     })
-     .then(function(data) {
-         window.location.replace("/members");
-     })
-     .catch(function(err) {
-         throw err;
-     })
- }
+    function signUpUser(email, password) {
+        $.post("/api/signup", {
+            email: email,
+            password: password
+        })
+            .then(function (data) {
+                window.location.replace("/login");
+            })
+            .catch(function (err) {
+                throw err;
+            })
+    }
 });
