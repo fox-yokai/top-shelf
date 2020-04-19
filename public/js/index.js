b44c101b30fd4fd64e1f7bc12b4111447c71d37c
fox-yokai <pat.w.cain@gmail.com>
2020-04-19 15:20:45 -0500

    social media sharing added

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $("body").floatingSocialShare({
+        counter: false,
+        buttons: ["facebook", "twitter", "telegram", "mail", "linkedin"],
+        text: {
+            "default": "share with:",
+            "facebook": "share with facebook",
+            "twitter": "tweet"
+        },
+        
+        });
+
+});
